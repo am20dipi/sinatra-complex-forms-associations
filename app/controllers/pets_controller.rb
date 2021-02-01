22816a999e0c98673e2e4aff7296b9f501b975ba
Adriana <am20dipi@alum.siena.edu>
2021-02-01 18:14:35 -0500

    commit tree ae2a19c777a5cf530943362eb556736307a439c4

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,10 +6,18 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @pet = Pet.create(id: params[:id], name: params[:id])
+
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
@@ -19,7 +27,23 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
   patch '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+
+    #binding.pry
+
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+
+    @pet.save
 
     redirect to "pets/#{@pet.id}"
   end
